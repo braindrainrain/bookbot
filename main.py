751a816836534f0fec6caa8939c035c65bd48934
rainier_crespo <rainier.crespo@gmail.com>
2025-06-01 19:27:57 -0500

    Print a Report

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from stats import word_counter, lowercase, get_book_text, sorted_count_letters
 content = get_book_text("books/frankenstein.txt")
 word_count = word_counter(content)
-print(f"There are {word_count} words in the document.")
 lower_text = lowercase(content)
-print(lower_text)
-print(f"''t': {character_counters['t']}'")
-print(f"''p': {character_counters['p']}'")
-print(f"''c': {character_counters['c']}'")
-sorted_character_counts = sorted_count_letters(character_counts)
+sorted_character_counts = sorted_count_letters(lower_text)
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+print(f"Found {word_count} total words")
+print("--------- Character Count -------")
 for char_data in sorted_character_counts:
     if char_data["char"].isalpha():
-        print(f"{char_data["char"]}:{char_data["num"]}")
+        print(f"{char_data["char"]}: {char_data["num"]}")
+print("============= END ===============")
